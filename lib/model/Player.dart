8f07da33423de4dcfcfa237c1d6ed04f8de998ef
Vika Belskaya <vika.bel773@gmail.com>
2022-03-09 21:50:04 +0300

    list<Player>

--- lib/model/Player.dart
+++ lib/model/Player.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: file_names, unnecessary_new
 class Player {
+  int? id;
   String? name;
   String? department;
 
-  Player({this.name, this.department});
+  Player({this.id, this.name, this.department});
 
   bool checkIfAnyIsNull() {
     return [name, department].contains(null);
   }
+
+  static List<Player> players = [];
 }
