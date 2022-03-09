cecec84a340fc67413f80aeb0aed6ae0d23141cb
Vika Belskaya <vika.bel773@gmail.com>
2022-03-09 21:50:52 +0300

    new class for points one game

---
+++ lib/model/Point.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: non_constant_identifier_names, file_names
+
+class Point {
+  int? id;
+  int? point_one;
+  int? point_two;
+
+  Point({this.id, this.point_one, this.point_two});
+}
