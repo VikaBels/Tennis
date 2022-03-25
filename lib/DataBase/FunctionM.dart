498732a014a5bf77c693c37141e5c93f9c27d11d
Vika Belskaya <vika.bel773@gmail.com>
2022-03-25 15:09:22 +0300

    add left join

--- lib/DataBase/FunctionM.dart
+++ lib/DataBase/FunctionM.dart
@@ -17,15 +17,54 @@ class FuncMatch {
     return matchList;
   }
 
-  /*Future<List> queryAll() async {
+  /*Future<String> queryAll() async {
     Database db = await DatabaseHelper.instance.database;
-    List<Map> names = await db.rawQuery(
-        'select PERSON.name, count(MATCH.id_one) from PERSON left join Date using(id) group by PEROSN.name');
-    if (names.length > 0) {
-      return names;
-    }
-    return [];
+
+    var name = await db.rawQuery(
+        '''Select p1.name as first_player, p2.name as second_player, m.point_one, m.point_two from Match m
+          Left join Person p1
+          On m.id_one=p1.id
+          Left join Person p2
+          On m.id_two=p2.id''');
+    //return name.last['name'].toString();
+    String
+        n = /*name.last['first_player'].toString() +
+        ' ' +
+        name.last['second_player'].toString() +*/
+        name.elementAt(0)['first_player'].toString();
+    return n;
   }*/
+  /* Future<List<dynamic>> queryAllForTwo() async {
+    Database db = await DatabaseHelper.instance.database;
+
+    var inf = await db.rawQuery(
+        '''Select p1.name as first_player, p2.name as second_player, m.point_one, m.point_two, m.time from Match m
+          Left join Person p1
+          On m.id_one=p1.id
+          Left join Person p2
+          On m.id_two=p2.id''');
+    List<dynamic> infList = inf.isNotEmpty ? inf.toList() : [];
+    return infList;
+  }*/
+
+  Future<List<dynamic>> queryAllForAll() async {
+    Database db = await DatabaseHelper.instance.database;
+
+    var inf = await db.rawQuery(
+        '''Select p1.name as first_player, p2.name as second_player, p3.name as third_player, p4.name as fourth_player,
+          m.point_one, m.point_two, m.time from Match m
+          Left join Person p1
+          On m.id_one=p1.id
+          Left join Person p2
+          On m.id_two=p2.id
+          Left join Person p3
+          On m.id_three=p3.id
+          Left join Person p4
+          On m.id_four=p4.id
+          ''');
+    List<dynamic> infList = inf.isNotEmpty ? inf.toList() : [];
+    return infList;
+  }
 
   Future<int> addMat(Match match) async {
     Database db = await DatabaseHelper.instance.database;
